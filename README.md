cef27be8d190a3bd11bd28d9272d712e2b823cdc
Jigneshkumar Parekh <jigneshparekh.it@gmail.com>
2024-11-10 19:32:41 -0800

    Added new file for Llama3 LLM query + Updated Readme

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ This reporsitory contains an example code of Amazon Bedrock with node.js
 - AWS Access Key and Secret Key generated and configured in your bash file like `~/.zshrc` or `.bashprofile` with following environement variables:
   - AWS_ACCESS_KEY_ID: <YOUR_AWS_ACCESS_KEY>
   - AWS_SECRET_ACCESS_KEY: <YOUR_AWS_SECRET_ACCESS_KEY>
+- Access to the AI21 Jamba-instruct model in your AWS console.
 
 ### Usage:
 
@@ -17,19 +18,19 @@ The bot is instructed to answer questions about provided context only and asked 
 Here how you can use:
 
 ```
-node invoke-model.js --query "Tell me about the things to do in Yosemite National Park." --file ./data/yosemite.txt
+node ai21-model.js --query "Tell me about the things to do in Yosemite National Park." --file ./data/yosemite.txt
 ```
 
 --- OR ---
 
 ```
-node invoke-model.js --query "Tell me about the different types of dogs" --file ./data/dogs.txt
+node llama-model.js --query "Tell me about the different types of dogs" --file ./data/dogs.txt
 ```
 
 If you ask any unrelated question, then it won't answer:
 
 ```
 
-node invoke-model.js --query "Tell me the best practices on JS" --file ./data/yosemite.txt
+node llama-model.js --query "Tell me the best practices on JS" --file ./data/yosemite.txt
 
 ```
---
+++ llama-model.js
@@ -0,0 +1,107 @@
+import {
+  BedrockRuntimeClient,
+  ConverseCommand,
+} from "@aws-sdk/client-bedrock-runtime";
+import fs from "node:fs/promises";
+import chalk from "chalk";
+import util from "node:util";
+import { typeWriter } from "./typewriter.js";
+
+async function invokeModel(prompt, modelId) {
+  // Create a new Bedrock Runtime client instance.
+  const client = new BedrockRuntimeClient({
+    region: "us-east-1",
+    // configure creds here.
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    },
+  });
+
+  const conversation = [
+    {
+      role: "user",
+      content: [{ text: prompt }],
+    },
+  ];
+
+  // Create a command with the model ID, the message, and a basic configuration.
+  const command = new ConverseCommand({
+    modelId,
+    messages: conversation,
+    inferenceConfig: { maxTokens: 512, temperature: 0.5, topP: 0.9 },
+  });
+
+  const apiResponse = await client.send(command);
+
+  // Extract the response text.
+  const responseText = apiResponse.output.message.content[0].text;
+  return responseText;
+}
+
+async function readFileContent(filePath = "./data/yosemite.txt") {
+  const content = await fs.readFile(filePath, "utf8");
+  return content;
+}
+
+function processArguments() {
+  // Accept the query from user
+  const args = process.argv.slice(2);
+  const options = {
+    query: {
+      type: "string",
+      short: "q",
+    },
+    file: {
+      type: "string",
+      short: "f",
+    },
+  };
+  const { values } = util.parseArgs({ args, options });
+  const { query, file } = values;
+
+  if (!query) {
+    console.log(
+      chalk.red(
+        'Missing input. Please provide "--query". For ex: --query "Tell me about things to do in Yosemite"'
+      )
+    );
+    process.exit(0);
+  }
+
+  if (!file) {
+    console.log(
+      chalk.red(
+        'Missing input. Please provide "--file". For ex: --file "./data/yosemite.txt"'
+      )
+    );
+    process.exit(0);
+  }
+
+  return { query, file };
+}
+
+async function main() {
+  const { query, file } = processArguments();
+  const prompt = `
+    Context: 
+      You're an assitive bot helping me learn about following context. Only answer if question is related to the privided context. Do not answer unrelated questions. 
+  
+      ${await readFileContent(file)}
+    
+    Question: ${query}
+  `;
+  const modelId = "meta.llama3-8b-instruct-v1:0";
+  console.log(`Prompt: ${prompt}`);
+  console.log(chalk.redBright(`Model ID: ${modelId}`));
+
+  try {
+    console.log("-".repeat(53));
+    const response = await invokeModel(prompt, modelId);
+    typeWriter(chalk.green(`Answer: \n${response}\n`));
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+main().catch((error) => console.log(chalk.red(`--> ERROR:`, error)));
